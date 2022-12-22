66fc6e01ce0e84fdba6240751b066993d0d1115b
Technocean <117555665+mail4metablocks@users.noreply.github.com>
2022-12-22 11:13:54 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,40 @@ struct NginxLog {
     http_user_agent: String,
 }
 
+fn filter_logs(logs: &[NginxLog], start_date: Option<DateTime<Local>>, end_date: Option<DateTime<Local>>, status: Option<u16>, referer: Option<&str>, path: Option<&str>) -> Vec<NginxLog> {
+    logs.into_iter()
+        .filter(|log| {
+            if let Some(start_date) = start_date {
+                if log.request_time < start_date {
+                    return false;
+                }
+            }
+            if let Some(end_date) = end_date {
+                if log.request_time > end_date {
+                    return false;
+                }
+            }
+            if let Some(status) = status {
+                if log.status != status {
+                    return false;
+                }
+            }
+            if let Some(referer) = referer {
+                if !log.http_referer.contains(referer) {
+                    return false;
+                }
+            }
+            if let Some(path) = path {
+                if !log.request.contains(path) {
+                    return false;
+                }
+            }
+            true
+        })
+        .cloned()
+        .collect()
+}
+
 fn parse_nginx_log_line(line: &str) -> Option<NginxLog> {
     let re = Regex::new(NGINX_LOG_FORMAT).unwrap();
     let captures = match re.captures(line) {
@@ -102,8 +136,25 @@ fn read_nginx_logs<P: AsRef<Path>>(path: P) -> Result<Vec<NginxLog>, Box<dyn Err
     Ok(logs)
 }
 
+fn trend_analysis(logs: &[NginxLog]) -> HashMap<String, u64> {
+    let mut trends = HashMap::new();
+    for log in logs {
+        let key = log.request_time.format("%Y-%m-%d").to_string();
+        *trends.entry(key).or_insert(0) += 1;
+    }
+    trends
+}
+
+
 fn main() -> Result<(), Box<dyn Error>> {
     let logs = read_nginx_logs("/path/to/logs")?;
-    print_logs(&logs);
+    let start_date = Local.ymd(2022, 1, 1).and_hms(0, 0, 0);
+    let end_date = Local.ymd(2022, 1, 31).and_hms(0, 0, 0);
+    let filtered_logs = filter_logs(&logs, Some(start_date), Some(end_date), Some(200), None, None);
+    let trends = trend_analysis(&filtered_logs);
+    for (date, count) in trends {
+        println!("{}: {}", date, count);
+    }
     Ok(())
 }
+
